505b0952c021302525bf3104bc8c5741990284bf
Karleeov <kingovlee@gmail.com>
2024-05-10 10:23:25 +0800

    Update theme.js

--- theme.js
+++ theme.js
@@ -3,23 +3,24 @@
         {
             "background": "#1F2430",
             "black": "#191E2A",
-            "blue": "#6DCBFA",
-            "brightBlack": "#686868",
-            "brightBlue": "#73D0FF",
-            "brightCyan": "#95E6CB",
-            "brightGreen": "#BAE67E",
-            "brightPurple": "#D4BFFF",
-            "brightRed": "#F28779",
+            "blue": "#6300ff", // Changed to (99,0,255)
+            "brightBlack": "#cc11f0", // Changed to (204,17,240)
+            "brightBlue": "#d14eea", // Changed to (209,78,234)
+            "brightCyan": "#00b8ff", // (0,184,255)
+            "brightGreen": "#00ff9f", // (0,255,159)
+            "brightPurple": "#bd00ff", // (189,0,255)
+            "brightRed": "#ff008d", // Changed to (255,0,141)
             "brightWhite": "#FFFFFF",
             "brightYellow": "#FFD580",
             "cursorColor": "#FFCC66",
-            "cyan": "#90E1C6",
+            "cyan": "#00b8ff", // (0,184,255)
             "foreground": "#CBCCC6",
-            "green": "#A6CC70",
-            "name": "Ayu Mirage",
-            "purple": "#CFBAFA",
-            "red": "#ED8274",
+            "green": "#00ff9f", // (0,255,159)
+            "name": "Ayu Mirage Custom",
+            "purple": "#bd00ff", // (189,0,255)
+            "red": "#d600ff", // (214,0,255)
             "selectionBackground": "#33415E",
             "white": "#C7C7C7",
-            "yellow": "#FAD07B"
-        },];
+            "yellow": "#f96363" // Changed to (249,99,99)
+        }
+        ];
